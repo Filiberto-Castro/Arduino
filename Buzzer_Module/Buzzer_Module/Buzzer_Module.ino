9aa386ec4b74482361ef2533ded15174a734833a
Filiberto_Castro <fcastrob@miumg.edu.gt>
2022-11-28 19:24:11 -0600

    Agregando comentarios

--- Buzzer_Module/Buzzer_Module/Buzzer_Module.ino
+++ Buzzer_Module/Buzzer_Module/Buzzer_Module.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 
+//Declarar el pin del Buzzer
 int buzzer = 25;
 
 void setup() {
@@ -8,14 +9,17 @@ pinMode(buzzer, OUTPUT);
 
 }
 void loop() {
-  
+
+//Imitir el sonido  
 tone(buzzer, 660);
 delay(250);
+//Apagar el sonido
 noTone();
 delay(500);
 
 }
 
+//Metodo para emitir el sonido
 int playing = 0;
 void tone(byte pin, int freq) {
   ledcSetup(0, 2000, 8);
@@ -23,6 +27,8 @@ void tone(byte pin, int freq) {
   ledcWriteTone(0, freq);
   playing = pin;
 }
+
+//Metodo para apagar el sonido
 void noTone() {
   tone(playing, 0);
 }
